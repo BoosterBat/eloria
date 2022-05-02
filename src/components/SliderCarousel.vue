14b1eed51faf472a925e87a3b0fb3ba7db1bf86e
Maximilian Schwarzmann <maxschwarzmann1009@gmail.com>
2022-05-02 11:16:41 +0200

    first commit

---
+++ src/components/SliderCarousel.vue
@@ -0,0 +1,69 @@
+<template>
+  <carousel :items-to-show="3.95" :wrapAround="true">
+    <slide v-for="session in sessions" :key="session.episode">
+      <div class="carousel__item">{{ session.title }}</div>
+    </slide>
+
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
+</template>
+
+<script>
+// https://vuejsexamples.com/modern-lightweight-vue-3-carousel-component/
+
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+import 'vue3-carousel/dist/carousel.css';
+
+import sessions from '../sessions.json'
+
+export default {
+  name: 'App',
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+  data() {
+    return {
+      sessions: sessions
+    }
+  }
+};
+</script>
+
+<style scoped>
+.carousel__slide > .carousel__item {
+  transform: scale(1);
+  opacity: 0.5;
+  transition: 0.5s;
+}
+.carousel__slide--visible > .carousel__item {
+  opacity: 1;
+  transform: rotateY(0);
+}
+.carousel__slide--next > .carousel__item {
+  transform: scale(0.9) translate(-10px);
+}
+.carousel__slide--prev > .carousel__item {
+  transform: scale(0.9) translate(10px);
+}
+.carousel__slide--active > .carousel__item {
+  transform: scale(1.1);
+}
+
+.carousel__item {
+    min-height: 200px;
+    width: 100%;
+    background-color: var(--vc-clr-primary);
+    color: var(--vc-clr-white);
+    font-size: 20px;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
